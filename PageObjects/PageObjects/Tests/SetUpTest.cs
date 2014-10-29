3d243ceb79d23ab0cfa7499d346e36c64c8df70b
Thejas007 <thejasgowda.n@gmail.com>
2014-10-29 16:01:11 +0530

    Update SetUpTest.cs
    Test added

--- PageObjects/PageObjects/Tests/SetUpTest.cs
+++ PageObjects/PageObjects/Tests/SetUpTest.cs
@@ -8,5 +8,34 @@ namespace PageObjects.Tests
 {
     class SetUpTest
     {
+        
+   [TestFixture]
+	public class SetUpTestWithPage
+	{
+		private string baseUrl;
+		private StartUpPage startUpPage;
+
+		public SetUpTestWithPage()
+		{
+			startUpPage = new StartUpPage(SeleniumWebDriver.Browser.Chrome);
+			baseUrl = "http://127.0.0.1/";
+		}
+
+
+		[Test]
+		public void OpenProjectWebPage()
+		{
+			startUpPage.OpenUrl(baseUrl + "Search/Index.html");
+			startUpPage.EnterSurName("john");
+			startUpPage.ClickSubmit();
+			startUpPage.VerifyResult();
+			
+		}
+		[TestFixtureTearDown]
+		public void EndTest()
+		{
+			startUpPage.Close();
+			
+		}
     }
 }
