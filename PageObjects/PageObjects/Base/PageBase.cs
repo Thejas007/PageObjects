edbb6084d13529aadbd6f3d5b038ca9dfdb99fcb
Thejas007 <thejasgowda.n@gmail.com>
2014-10-29 16:00:12 +0530

    Update PageBase.cs
    Added First setup

--- PageObjects/PageObjects/Base/PageBase.cs
+++ PageObjects/PageObjects/Base/PageBase.cs
@@ -8,5 +8,36 @@ namespace PageObjects
 {
     public class PageBase
     {
+       protected FluentAutomation.Interfaces.INativeActionSyntaxProvider I { get; private set; }
+
+		protected FluentTest FluentTest;
+		public PageBase(SeleniumWebDriver.Browser browser)
+		{
+			SeleniumWebDriver.Bootstrap(browser);
+			FluentTest = new FluentTest();
+			I = FluentTest.I;
+
+		}
+		public PageBase()
+		{
+			SeleniumWebDriver.Bootstrap(SeleniumWebDriver.Browser.Chrome);
+			FluentTest = new FluentTest();
+			I = FluentTest.I;
+
+		}
+
+		/** Open  */
+		public void OpenUrl(string url)
+		{
+			I.Open(url);
+            I.Wait(5);
+		}
+
+		public void Close()
+		{
+			I.Dispose();
+
+		}
+
     }
 }
